1d8d8756f6871a287f23e873af97140b9def061c
Sinan Taga <sinantaga@gmail.com>
2013-01-24 23:14:42 +0200

    Initial commit

---
+++ counter.js
@@ -0,0 +1,51 @@
+// Extremely Simple Countdown jQery Plugin
+
+(function( $ ) {
+    var secondsPerMinute    = 60,
+        secondsPerHour      = 3600,
+        secondsPerDay       = 86400,
+        secondsPerWeek      = 604800;
+    
+
+    $.fn.simpleCountdown = function(options) {
+        // Create some defaults, extending them with any options that were provided
+        var settings = $.extend( {
+                'showSeconds' : true,
+                'showZero' : true
+            }, options);
+
+        var $this = this,
+            targetDate = new Date(2013, 1, 25);
+
+
+        (function tick(){
+            var timeLeft = Math.floor((targetDate - (new Date())) / 1000), // time left in seconds
+                w, d, h, m, s;
+
+            if (timeLeft > 0) {
+
+                // Number of days
+                d = Math.floor(timeLeft / secondsPerDay);
+                timeLeft -= d * secondsPerDay;
+
+                // Number of hours left
+                h = Math.floor(timeLeft / secondsPerHour);
+                timeLeft -= h * secondsPerHour;
+
+                // Number of minutes left
+                m = Math.floor(timeLeft / secondsPerMinute);
+                timeLeft -= m * secondsPerMinute;
+
+                // Number of seconds left
+                s = timeLeft;
+                
+                $this.html(d + ' gün ' + h + ' saat ' + m + ' dakika ' + s);
+            };
+
+            setInterval(tick, 1000);
+        })();
+
+        
+
+    };
+})( jQuery );
